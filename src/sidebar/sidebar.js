6e4492a17a0cc8ce406933a5e6f59c2f4d340735
JaydenPahukula <jayden.pahukula@gmail.com>
2025-09-18 20:29:11 -0500

    sidebar complete for now

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -2,6 +2,9 @@ import "./sidebar.css";
 
 const sidebar = document.getElementById("sidebar");
 
-// sidebar.addEventListener("click", () => {
-// 	sidebar.classList.toggle("collapsed");
-// });
+function toggleSidebar() {
+	sidebar.classList.toggle("collapsed");
+}
+
+document.getElementById("sidebar-header-menu-button").addEventListener("click", toggleSidebar);
+document.getElementById("sidebar-header-x-button").addEventListener("click", toggleSidebar);
